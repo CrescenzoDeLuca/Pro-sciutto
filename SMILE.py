64f6b2f0c2eba15ec6117244d6c5d315e8b445e3
CrescenzoDeLuca <lmcdfa@gmail.com>
2018-10-20 11:26:59 +0200

    BESTEMMIE

--- SMILE.py
+++ SMILE.py
@@ -1,10 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.title ("yuppa!!")
+def Prov(*args):
+     msg_entry.set(msg_entry.get())
 
-mainframe = ttk.frame(root)
-mainframe.grid(0, 0)
+root=Tk()
+root.title("yuop")
+
+mainframe=ttk.Frame(root, padding="12 20 12 20")
+mainframe.grid(column=0, row=0)
+
+msg_entry= StringVar()
+ttk.Entry(mainframe, width=8,textvariable=msg_entry ).grid(column=0, row=5)
+ttk.Button(mainframe, text="la madonna è fatta di...", command=Prov).grid(column=0, row=0)
+
+ttk.Label(mainframe, text="ella", textvariable=msg_entry).grid(column=0, row=1)
 
 root.mainloop()
