0ded5a4ad5397bb3236e010d3133ce7f41972270
CrescenzoDeLuca <lmcdfa@gmail.com>
2018-11-18 13:20:27 +0100

    piu offensivo che mai

--- SMILE.py
+++ SMILE.py
@@ -2,18 +2,20 @@ from tkinter import *
 from tkinter import ttk
 
 def Prov(*args):
-     msg_entry.set(msg_entry.get())
+     msg_label.set(msg_entry.get())
+     msg_entry.set('')
 
 root=Tk()
-root.title("yuop")
+root.title("bestemminetor-2000")
 
 mainframe=ttk.Frame(root, padding="12 20 12 20")
 mainframe.grid(column=0, row=0)
 
 msg_entry= StringVar()
+msg_label=StringVar()
 ttk.Entry(mainframe, width=8,textvariable=msg_entry ).grid(column=0, row=5)
 ttk.Button(mainframe, text="la madonna è fatta di...", command=Prov).grid(column=0, row=0)
 
-ttk.Label(mainframe, text="ella", textvariable=msg_entry).grid(column=0, row=1)
+ttk.Label(mainframe, text="ella", textvariable=msg_label).grid(column=0, row=1)
 
 root.mainloop()
